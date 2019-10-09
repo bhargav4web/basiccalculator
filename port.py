d8503e33c7a5fd71528883a7566c6df9c0bfc281
Reveur <nitinvsr>
2019-10-09 21:42:35 +0530

    Added port.py

---
+++ port.py
@@ -0,0 +1,17 @@
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+# Clear the screen
+subprocess.call('clear', shell=True)
+
+# Ask for input
+remoteServer    = raw_input("Enter a remote host to scan: ")
+remoteServerIP  = socket.gethostbyname(remoteServer)
+
+# Print a nice banner with information on which host we are about to scan
+print "-" * 60
+print "Please wait, scanning remote host", remoteServerIP
+print "-" * 60
+
